b05e75aa2b199f0416bfe777d2120b9784e97213
YohannesTz <52599607+YohannesTz@users.noreply.github.com>
2021-10-30 08:44:38 +0300

    added Answer to worksheet one

---
+++ year1/semister2/main.cpp
@@ -0,0 +1,48 @@
+1, int cnt inch= 0;
+   int cnt feet=0;
+   
+2, double distance = 1.5;
+
+3,    int n1, n2, sum;
+
+4,    sum = n1 + n2;
+
+5,    double length;
+    length = length + 8.33;
+
+6,    int product, n;
+    product = product * n;
+
+7,    double speed, parRate, payRateHourly;
+    int highScore;
+
+8,    cout << "The answer to the question of Life, \n";
+    cout << "\n";
+    cout << "the universe and everything is 42.";
+
+9,    int _number;
+    cout << "Please input a whole number: \n";
+    cin >> _number;
+
+10,    cout << "Hello World";
+
+
+11,    int whole_num_1, whole_num_2, thier_sum;
+    cout << "Please input two whole numbers to add:\n";
+    cin >> whole_num_1;
+    cin >> whole_num_2;
+    thier_sum = whole_num_1 + whole_num_2;
+    cout << thier_sum;
+
+12, 
+    double one = 1.000;
+    double two = 1.414;
+    double three = 1.732;
+    double four = 2.000;
+    double five = 2.236;
+    cout << "N\tSquare root";
+    count << "1\t" << one;
+    count << "2\t" << two;
+    count << "3\t" << three;
+    count << "4\t" << four;
+    count << "5\t" << five;
